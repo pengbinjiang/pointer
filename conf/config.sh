15c5cb7e92f6759d25b227f9f810290155259a36
pengbinjiang <pengbinjiang22@gmail.com>
2017-11-10 19:25:13 +0800

    profile

---
+++ conf/config.sh
@@ -0,0 +1,11 @@
+#解决manjaro linux的中文输入
+#pacman -S fcitx fcitx-libpinyin kcm-fcitx
+#然后修改 .xprofile
+#cat xprofile >> ~/.xprofile
+
+#配置.vimrc
+cat vimrc >> ~/.vimrc
+
+#man手册显示 source .bashrc立即生效
+cat manrc >> ~/.bashrc
+
